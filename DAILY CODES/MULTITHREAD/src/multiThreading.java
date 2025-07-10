69b8a2d36bc3a4a8e195d842bafb7a2927a7f1b6
aryancodes12-bit <aryanjaiswal3080@gmail.com>
2025-07-10 17:26:42 +0530

    Added first code

---
+++ DAILY CODES/MULTITHREAD/src/multiThreading.java
@@ -0,0 +1,31 @@
+
+class MyThreadRun1 implements Runnable {
+    @Override
+    public void run() {
+        int i = 0;
+        while (i < 20000) {
+            System.out.println(" i am Thread1 ");
+            i++;
+        }
+    }
+}
+class MyThreadRun2 implements Runnable{
+    @Override
+    public void run() {
+        int i=0;
+        while(i<20000){
+            System.out.println(" i am Thread2 ");
+            i++;
+        }
+    }}
+
+public class multiThreading {
+    public static void main(String[] args) {
+        MyThreadRun1 bullet1=new MyThreadRun1();
+        Thread gun1=new Thread(bullet1);
+        MyThreadRun2 bullet2=new MyThreadRun2();
+        Thread gun2=new Thread(bullet2);
+        gun1.start();
+        gun2.start();
+    }
+}
